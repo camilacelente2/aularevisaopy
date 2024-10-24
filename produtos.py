a39c1308cdaf272edd6742aacc4bedd5f675db12
camilacelente2 <camilacelente@yahoo.com.br>
2024-10-23 23:50:59 -0300

    produtos.py

---
+++ produtos.py
@@ -0,0 +1,32 @@
+sair = True
+
+
+while sair == True :
+
+
+    nome_produto = str(input("Digite o  nome do  produto: "))
+    print()  
+    preco = float(input("digite o  peço: "))
+
+    print("Erro! digite um numero")
+    
+
+    if (preco <= 50) :
+        print("produtro de baixo  custo.")
+        print(nome_produto)
+        print(preco)
+        sair = False
+
+    elif(preco >50  and preco <=100 ) :
+
+        print("produtro de medio custo.")
+        print(nome_produto)
+        print(preco)
+        sair = False
+
+    else :
+        print("produtro de alto custo.")
+        print(nome_produto)
+        print(preco)
+        sair = False
+ 
